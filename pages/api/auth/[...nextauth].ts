d982e435d830594b6c5c4fe6f82de54886423b8d
Ethan0908 <44311213+Ethan0908@users.noreply.github.com>
2025-10-04 13:59:06 -0700

    Normalize Next.js config formatting

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/gmail.send",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
