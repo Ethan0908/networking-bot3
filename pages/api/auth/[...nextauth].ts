83d82ff0e8b66c2593d2684a59b94931b95cba2c
Ethan0908 <44311213+Ethan0908@users.noreply.github.com>
2025-10-04 00:28:20 -0700

    Fix NextAuth Google scope configuration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,12 @@ export default NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      scope: "openid email profile https://www.googleapis.com/auth/gmail.send",
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/gmail.send",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
