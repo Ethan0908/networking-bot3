51cf692e69bcc6d5cee2ef1131433104755192a7
Ethan0908 <44311213+Ethan0908@users.noreply.github.com>
2025-10-12 18:53:35 -0700

    Add legacy /api/jobs aliases for progress and done

---
+++ app/api/jobs/[jobId]/done/route.ts
@@ -0,0 +1,2 @@
+export { runtime } from "../../../email-jobs/[jobId]/done/route";
+export { POST } from "../../../email-jobs/[jobId]/done/route";
---
+++ app/api/jobs/[jobId]/progress/route.ts
@@ -0,0 +1,2 @@
+export { runtime } from "../../../email-jobs/[jobId]/progress/route";
+export { POST } from "../../../email-jobs/[jobId]/progress/route";
